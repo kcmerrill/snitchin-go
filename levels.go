9ddaba66220b1c2edd066bf92d1dff082825027a
kc merrill <kcmerrill@gmail.com>
2016-03-01 18:43:36 +0000

    Enhancements: CLI Log levels  - Now allows ability to do -vvvvvvv etc etc

--- levels.go
+++ levels.go
@@ -1,8 +1,12 @@
 package snitchin
 
 import (
+	"flag"
+	"fmt"
 	"strings"
 )
+
+var verbosity map[int]*bool
 
 type level struct {
 	name   string
@@ -11,6 +15,25 @@ type level struct {
 }
 
 var levels map[string]*level = make(map[string]*level)
+
+func CLIFlags() {
+	verbosity = make(map[int]*bool)
+	vees := ""
+	/* Setup our --vvvvvv array */
+	for x := 700; x >= 0; x -= 100 {
+		vees += "v"
+		verbosity[x] = flag.Bool(vees, false, fmt.Sprintf("Log verbosity level set to %d", x))
+	}
+}
+
+func CLILogLevel() int {
+	for level, f := range verbosity {
+		if *f {
+			return level
+		}
+	}
+	return 0
+}
 
 func Level(name string) (string, int, string) {
 	name = strings.ToUpper(name)
